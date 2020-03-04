83d8f6018e319511f46ad83ccfe37c9537fb4655
kwizl <karis.njoroge@gmail.com>
2020-03-04 23:06:28 +0300

    Wrote the pseudo code for the game logic

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+git_source(:github) { "https://github.com/#{repo_name}" }
+
+gem 'colorize', '~> 0.8.1'
+gem 'tty-screen'
---
+++ bin/main.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+p 'Welcome! This is Tic Tac Toe Game. Have Fun!'
+
+puts 'Player One: X'
+puts '2D dimensional display for tit tac toe board'
+print 'Choose a number: '
+choiceOne = gets.chomp
+
+puts 'Player Two: O'
+puts 'Updated 2D dimensional display for tit tac toe board'
+print 'Choose a number: '
+choiceTwo = gets.chomp
+
+puts 'Player Two: X'
+puts 'Updated 2D dimensional display for tit tac toe board'
+print 'Choose a number: '
+choiceTwo = gets.chomp
+
+puts 'Player Two: O'
+puts 'Updated 2D dimensional display for tit tac toe board'
+print 'Choose a number: '
+choiceTwo = gets.chomp
+
+puts 'Player Two: X'
+puts 'Updated 2D dimensional display for tit tac toe board'
+print 'Choose a number: '
+choiceTwo = gets.chomp
+
+puts 'Player Two: O'
+puts 'Updated 2D dimensional display for tit tac toe board'
+print 'Choose a number: '
+choiceTwo = gets.chomp
