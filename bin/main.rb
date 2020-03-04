fc2d42e8f99369f527e2ee476cf86b8a254d9a8e
kwizl <karis.njoroge@gmail.com>
2020-03-05 00:15:24 +0300

    Rectifying linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -12,43 +12,52 @@ puts "#{player_one}: X"
 puts '2D dimensional display for tic tac toe board'
 print 'Choose a number: '
 choice_one = gets.chomp
+puts "Number: #{choice_one}"
 
 puts "#{player_two}: O"
 puts 'Updated 2D dimensional display for tic tac toe board'
 print 'Choose a number: '
 choice_two = gets.chomp
+puts "Number: #{choice_two}"
 
 puts "#{player_one}: X"
 puts 'Updated 2D dimensional display for tic tac toe board'
 print 'Choose a number: '
 choice_three = gets.chomp
+puts "Number: #{choice_three}"
 
 puts "#{player_two}: O"
 puts 'Updated 2D dimensional display for tic tac toe board'
 print 'Choose a number: '
 choice_four = gets.chomp
+puts "Number: #{choice_four}"
 
 puts "#{player_one}: X"
 puts 'Updated 2D dimensional display for tic tac toe board'
 print 'Choose a number: '
 choice_five = gets.chomp
+puts "Number: #{choice_five}"
 
 puts "#{player_two}: O"
 puts 'Updated 2D dimensional display for tic tac toe board'
 print 'Choose a number: '
 choice_six = gets.chomp
+puts "Number: #{choice_six}"
 
 puts "#{player_one}: X"
 puts 'Updated 2D dimensional display for tic tac toe board'
 print 'Choose a number: '
 choice_seven = gets.chomp
+puts "Number: #{choice_seven}"
 
 puts "#{player_two}: O"
 puts 'Updated 2D dimensional display for tic tac toe board'
 print 'Choose a number: '
-choice_six = gets.chomp
+choice_eight = gets.chomp
+puts "Number: #{choice_eight}"
 
 puts "#{player_one}: X"
 puts 'Updated 2D dimensional display for tic tac toe board'
 print 'Choose a number: '
-choice_seven = gets.chomp
+choice_nine = gets.chomp
+puts "Number: #{choice_nine}"
