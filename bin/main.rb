93bc1747b20e46377aa517f8ad8c3b6df481bee1
alfred noble <ezakaphil@gmail.com>
2020-03-05 17:33:45 +0100

    implement game logic with a loop

--- bin/main.rb
+++ bin/main.rb
@@ -8,56 +8,27 @@ puts "#{player_one} you'll play as X"
 player_two = gets.chomp
 puts "#{player_two} you'll play as O"
 
-puts "#{player_one}: X"
-puts '2D dimensional display for tic tac toe board'
-print 'Choose a number: '
-choice_one = gets.chomp
-puts "Number: #{choice_one}"
+start_game = true
+slots = 1
 
-puts "#{player_two}: O"
-puts 'Updated 2D dimensional display for tic tac toe board'
-print 'Choose a number: '
-choice_two = gets.chomp
-puts "Number: #{choice_two}"
+player_one_choices = []
+player_two_choices = []
 
-puts "#{player_one}: X"
-puts 'Updated 2D dimensional display for tic tac toe board'
-print 'Choose a number: '
-choice_three = gets.chomp
-puts "Number: #{choice_three}"
+while start_game
+  puts "#{player_one}, choose from any available slot(s), from 1 to 9"
+  choice_one = gets.chomp
+  player_one_choices.push(choice_one)
+  puts "Mark number-slot chosen by #{player_one} as X on the canvas"
+  slots += 1
 
-puts "#{player_two}: O"
-puts 'Updated 2D dimensional display for tic tac toe board'
-print 'Choose a number: '
-choice_four = gets.chomp
-puts "Number: #{choice_four}"
+  puts "#{player_two}, choose from any available slot(s), from 1 to 9"
+  choice_two = gets.chomp
+  player_two_choices.push(choice_two)
+  puts "Mark number-slot chosen by #{player_two} as O on the canvas"
+  slots += 1
 
-puts "#{player_one}: X"
-puts 'Updated 2D dimensional display for tic tac toe board'
-print 'Choose a number: '
-choice_five = gets.chomp
-puts "Number: #{choice_five}"
+  puts "<<<<<slot is #{slots} >>>>"
+  slots == 9 ? start_game = false : true
+end
 
-puts "#{player_two}: O"
-puts 'Updated 2D dimensional display for tic tac toe board'
-print 'Choose a number: '
-choice_six = gets.chomp
-puts "Number: #{choice_six}"
-
-puts "#{player_one}: X"
-puts 'Updated 2D dimensional display for tic tac toe board'
-print 'Choose a number: '
-choice_seven = gets.chomp
-puts "Number: #{choice_seven}"
-
-puts "#{player_two}: O"
-puts 'Updated 2D dimensional display for tic tac toe board'
-print 'Choose a number: '
-choice_eight = gets.chomp
-puts "Number: #{choice_eight}"
-
-puts "#{player_one}: X"
-puts 'Updated 2D dimensional display for tic tac toe board'
-print 'Choose a number: '
-choice_nine = gets.chomp
-puts "Number: #{choice_nine}"
+puts 'win or draw situation'
